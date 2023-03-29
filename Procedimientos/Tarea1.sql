9a2f41d3f42d9632764199f47a7c6fc5e693d753
AngelPRO <angelchiquitopons@gmail.com>
2023-03-29 18:41:24 +0100

    29-03

---
+++ Procedimientos/Tarea1.sql
@@ -0,0 +1,20 @@
+/*
+DELIMITER // -- comienzo del delimitador
+CREATE PROCEDURE nombre_procedimiento
+BEGIN
+  //codigo
+END //  -- fin del delimitador
+*/
+
+
+/*Sacar una relación completa de los científicos asignados a cada proyecto. Mostrar DNI, Nombre del científico, 
+identificador del proyecto y nombre del proyecto.*/
+
+/*Obtener el número de proyectos al que está asignado cada científico (mostrar el DNI y el nombre).*/
+
+/*Obtener el numero de científicos asignados a cada proyecto (mostrar el identificador del proyecto y el nombre del proyecto).*/
+
+/*Obtener el número de horas de dedicación de cada científico.*/
+
+/*Obtener el DNI y nombre de los científicos que se dedican a más de un proyecto y cuya dedicación media a cada 
+proyecto sea superior a un número de horas superior a 10, por ejemplo 11 horas.*/
